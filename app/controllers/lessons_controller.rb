b974b0cb4301b73628c446aeb12aa35306768f25
Charlie Tyler <charlietyler@live.com>
2018-01-19 12:29:20 +0000

    Added stripe payments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      return redirect_to course_path(current_lesson.section.course), alert: 'You must be Enrolled to view course lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
